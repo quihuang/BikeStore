20b843bbc05340b0e71c2234463051ff8cbf47ba
quihuang <quihuang2017@gmail.com>
2022-08-28 17:06:20 -0500

    Se cambia el diagrama de clases y se crea clase de venta en el modelo de dominio

---
+++ BikeStore.App/BikeStore.App.Dominio/Entidades/Venta.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BikeStore.App.Dominio
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public Date Fecha { get; set; }
+        public int CantidadProducto { get; set; }
+        public int ValorVenta { get; set; }
+        public Comercial Comercial { get; set; }
+        public Cliente Cliente { get; set; }
+        public Inventario Inventario { get; set; }
+    }
+}
