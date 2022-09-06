f45445ae4b4a1305eb019e5fe96c95d6a4f796fe
DairoCamacho <dairoleandro83@gmail.com>
2022-09-05 22:18:37 -0500

    add interface de Producto en Persistencia

---
+++ BikeStore.App/BikeStore.App.Persistencia/AppRepositorio/RepositorioProducto.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BikeStore.App.Dominio;
+
+namespace BikeStore.App.Persistencia
+{
+    public class RepositorioPersona : IRepositorioProducto
+    {
+
+        private readonly AppContext _appContext;
+
+        public RepositorioProducto(AppContext context){
+            _appContext = context;
+        }
+
+        // Método para buscar todos los registros
+        IEnumerable<Producto> IRepositorioProducto.GetAllProducto(){
+             return _appContext.Productos;
+        }
+
+        // Método para buscar por id
+        Producto IRepositorioProducto.GetProducto(int id){
+            return _appContext.Productos.Find(id);
+        }
+
+        // Método para buscar por nombre
+        IEnumerable<Producto> IRepositorioProducto.GetAllProductoForName (string name) {
+            var producto = _appContext.Productos.Where( p => p.Nombre == name );
+            //var producto = _appContext.Productos.Where( p => p.Nombre.contains(name));
+            return producto;
+        }
+
+        // Método para Crear un Producto
+        int IRepositorioProducto.AddProducto(Producto producto){
+            var productoLocal = _appContext.Producto.Add(producto);
+            var result = _appContext.SaveChanges();
+            return result;
+        }
+
+        // Método para Actualizar un Producto
+        int IRepositorioProducto.UpdateProducto(Producto producto){
+            _appContext.Productos.Update(producto);
+            return _appContext.SaveChanges();
+        }
+
+        // Método para Eliminar un Producto
+        int IRepositorioProducto.DeleteProducto(Producto producto){
+            _appContext.Productos.Remove(producto);
+            return _appContext.SaveChanges();
+        }
+    }
+}
